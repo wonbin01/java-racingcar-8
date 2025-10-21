8047a0806f324d51bd017f80d6f7b9711122fcf9
wonbin <wonbin3977@gmail.com>
2025-10-22 03:27:58 +0900

    feat(Handler) : 사용자의 입력을 쉼표(,)를 기준으로 구분하는 기능 구현

--- src/main/java/racingcar/handler/InputHandler.java
+++ src/main/java/racingcar/handler/InputHandler.java
@@ -1,14 +1,22 @@
 package racingcar.handler;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputHandler {
-    private Console console;
+    public List<String> racingCars;
 
     public void startInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = console.readLine();
-
+        String input = Console.readLine();
+        racingCars = Arrays.asList(delimiter(input));
     }
 
+    public String[] delimiter(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력이 비어있습니다.");
+        }
+        return input.split(",");
+    }
 }
