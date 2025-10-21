d40eba0cd621a1741c73dda53feb89b1bbf26a56
wonbin <wonbin3977@gmail.com>
2025-10-22 03:09:29 +0900

    feat(Handler) : 사용자로부터 이름 입력받기 구현

---
+++ src/main/java/racingcar/handler/InputHandler.java
@@ -0,0 +1,14 @@
+package racingcar.handler;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputHandler {
+    private Console console;
+
+    public void startInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = console.readLine();
+
+    }
+
+}
