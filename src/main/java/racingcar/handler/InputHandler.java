0f9945eb683f36cfd102e3268c6d7a76beba83e1
wonbin <wonbin3977@gmail.com>
2025-10-25 18:32:46 +0900

    refactor(InputHadnler) : 메서드 이름 변경, start 메서드

--- src/main/java/racingcar/handler/InputHandler.java
+++ src/main/java/racingcar/handler/InputHandler.java
@@ -11,7 +11,7 @@ public class InputHandler {
     InputException ex = new InputException();
     Racing race;
 
-    public void startInput() {
+    public void start() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         cars = Arrays.asList(ex.delimiter(input));
