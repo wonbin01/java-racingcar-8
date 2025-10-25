ef4c67f2ca12fb180feff68879be8aa0898cea00
wonbin <wonbin3977@gmail.com>
2025-10-25 16:28:04 +0900

    refactor(Car) : createCar메서드 삭제 및 Racing 클래스 생성

---
+++ src/main/java/racingcar/entity/Racing.java
@@ -0,0 +1,8 @@
+package racingcar.entity;
+
+import java.util.List;
+
+public class Racing {
+    private List<Car> cars;
+
+}
--- src/main/java/racingcar/handler/InputHandler.java
+++ src/main/java/racingcar/handler/InputHandler.java
@@ -1,15 +1,12 @@
 package racingcar.handler;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import racingcar.entity.Car;
 import racingcar.validator.InputException;
 
 public class InputHandler {
     public List<String> cars;
-    public List<Car> racingCars = new ArrayList<>();
     InputException ex = new InputException();
 
     public void startInput() {
@@ -17,15 +14,8 @@ public class InputHandler {
         String input = Console.readLine();
         cars = Arrays.asList(ex.delimiter(input));
         ex.validateInput(cars);
-        createRacingCars();
         System.out.println("시도할 횟수는 몇 회인가요?");
         input = Console.readLine();
     }
 
-    private void createRacingCars() {
-        for (String car : cars) {
-            Car carObj = new Car(car);
-            racingCars.add(carObj);
-        }
-    }
 }
