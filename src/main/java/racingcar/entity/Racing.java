78850c2ba864630616e1e63a775de7c3db4aff9b
wonbin <wonbin3977@gmail.com>
2025-10-25 16:57:48 +0900

    feat(Racing) : startRace, raceOnce, printStatus 메서드 구현
    startRace : 레이스 시작하는 메서드
    
    raceOnce : 램덤 변수를 생성해서, 4이상이면 distance++, 각 라운드마다 거리 계산
    
    printStatus : 각 단계마다 자동차의 상태를 출력

--- src/main/java/racingcar/entity/Racing.java
+++ src/main/java/racingcar/entity/Racing.java
@@ -1,8 +1,55 @@
 package racingcar.entity;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Racing {
     private List<Car> cars;
+    private int chance;
+    private List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    public Racing(List<String> carname, int number) {
+        cars = new ArrayList<>();
+        for (String name : carname) {
+            cars.add(new Car(name));
+        }
+        this.chance = number;
+    }
+
+    public void startRace() {
+        System.out.println("실행 결과");
+        for (int i = 0; i < chance; i++) {
+            raceOnce();
+        }
+    }
+
+    public void raceOnce() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInList(numbers);
+            if (randomNumber >= 4) {
+                car.incrementDistance();
+            }
+        }
+    }
+
+    public void printStatus() {
+        for (Car car : cars) {
+            Long distance = car.getDistance();
+            String name = car.getCarName();
+            String mark = createDistance(distance);
+            System.out.println(name + " : " + mark);
+        }
+        System.out.println("");
+    }
+
+    public String createDistance(Long number) {
+        StringBuilder distance = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            distance.append("-");
+        }
+        return distance.toString();
+    }
 
 }
